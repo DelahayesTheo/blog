de4f81ed8bb4c647fc65fde3a95eccaa12891a81
Ludovic Frank <SilentKernel@users.noreply.github.com>
2017-07-15 00:21:51 +0200

    Trowel instead of bootstrap, added normalize.css, modified header

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,7 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableSassLoader()
     .enableSourceMaps(!Encore.isProduction())
-    // Vendors
-    .createSharedEntry('vendor', [
-        'jquery',
-        'bootstrap',
 
-        // you can also extract CSS - this will create a 'vendor.css' file
-        // this CSS will *not* be included in page1.css or page2.css anymore
-        './assets/css/vendor.scss'
-    ])
     // Custom Style
     .addStyleEntry('main', './assets/css/main.scss')
 
